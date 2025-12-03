a2213ced7f3676e35e134bc9bf41ed44c67b35e9
A1_Alpha <164620731+A1Alpha003@users.noreply.github.com>
2025-12-03 20:55:12 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
+import os
 import threading
 from gui.dashboard import launch_gui
 from modules.log_monitor import LogMonitor
+import sqlite3
+
+def ensure_database():
+    if not os.path.exists("database"):
+        os.makedirs("database")
+
+    # Create empty forensic.db if missing
+    conn = sqlite3.connect("database/forensic.db")
+    conn.close()
 
 if __name__ == "__main__":
+    ensure_database()
+
     log_monitor = LogMonitor("/var/log/auth.log")
 
     t1 = threading.Thread(target=log_monitor.start_monitoring)
